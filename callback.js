eac8e53652298e373d0e006534b8b92777eef8d8
Gökan ŞİŞMAN <gknsismandenmark@gmail.com>
2021-02-16 15:31:31 +0200

    Update Abuzer Silinde

--- callback.js
+++ callback.js
@@ -17,6 +17,6 @@ loginol(listele);
 //NOT JS'de önce kolay işlemler yapılır sonra diğeri olur. Yani asenkron çalışır. 
 //Ancak Call back'te işlem sırasını belirliyorsun. 
 //İstediğin senkron olmasını yani sırayla yapılamını sağlar. 
-//Yukarıdaki örnekte şayet abuzer yazısı yani callback olmasaydı. 500 küçük olduğu için
+//Yukarıdaki örnekte şayet listele yazısı yani callback olmasaydı. 500 küçük olduğu için
 //önce listele çalışır, sonra 2000 olduğu için loginol çalışır.
 //Bir başka şeyde bu örnekte olduğu gibi şayet login başarılı ise listele çalışsın da denilebilir.
